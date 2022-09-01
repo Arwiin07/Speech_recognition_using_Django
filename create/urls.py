ae65111e1e194f0760e77fecf933bc4d8179c33c
Arwin.P <Arwin.p@kryptostech.com>
2022-09-01 12:08:11 +0530

    third commit

--- create/urls.py
+++ create/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.login,name="login"),
+    path('lists',views.lists,name="lists"),
+    path('choosing_between',views.choosing,name="choose"),
+    path('chosen_file/',views.second_page,name="chosen_file"),
     path('transcript/',views.index,name="index"),
     # path('next',views.audio_process,name="index"),
-    path('audiofile/process/', views.audio_process,name = 'audioprocess')
+    path('audiofile/process/', views.audio_process,name = 'audioprocess'),
+    path('<id>', views.detail_view,name="detail_view" ),
 ]
--- create/views.py
+++ create/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from . import audioprocess
-
+from . import audioprocess,models
 
 def index(request,*args,**kwargs):
-    # import pdb;pdb.set_trace()
+    import pdb;pdb.set_trace()
     if request.user.username==request.POST.get("Uname"):
-        return render(request,'index.html')
+        return render(request,'second_page.html')
     else:
         return render(request, 'login.html')
 
@@ -18,16 +17,46 @@ def audio_process(request):
     print(request.POST)
     #import pdb;pdb.set_trace()
     audio_dir = "D:\Mchine learning_integration\Arwin\Speech_recognition\media"+ "\\" + request.POST.get('filename')
-
-    data_out = audioprocess.upload(audio_dir)
-    transcript,out_score = audioprocess.speechrecog(data_out)
-    print(transcript,'sdjkh')
-
-
-
-
-    return render(request,'index.html',{'text_transcript':transcript,'score':out_score})
+    file_name1=request.POST.get('filename')
+    # import pdb;pdb.set_trace()
+    if models.Person.objects.filter(file_name=file_name1).exists():
+        return render(request,"second_page.html")
+    else:
+        data_out = audioprocess.upload(audio_dir)
+        transcript,out_score = audioprocess.speechrecog(data_out)
+        print(transcript,'sdjkh')
+    # import pdb;pdb.set_trace()
+    # if models.Person.objects.filter(file_name=file_name1).exists():
+        models.Person.objects.create(file_name=file_name1,transcripted_sentence=transcript,Sentiment_analysis=out_score)
+        return render(request,'index.html',{'text_transcript':transcript,'score':out_score,'file_name':file_name1})
 
 # @login_required
 def login(request):
     return render(request,"login.html")
+
+def second_page(request):
+    return render(request,'second_page.html')
+
+
+def choosing(request):
+    if request.user.username==request.POST.get("Uname"):
+        return render(request,'choosing.html')
+    else:
+        return render(request, 'login.html')
+    # return render(request,'choosing.html')
+
+def lists(request):
+    lists_of_transcriptions=models.Person.objects.all()
+    return render(request,"listing_db_elements.html",{'lists_of_transcripts':lists_of_transcriptions})
+
+
+def detail_view(request, id):
+    # dictionary for initial data with
+    # field names as keys
+    context = {}
+    # import pdb;pdb.set_trace()
+
+    # add the dictionary during initialization
+    context["data"] = models.Person.objects.get(id=id)
+
+    return render(request, "detail_view.html", context)
